8fbfb1fdbc7b9ad6608c346a339260d08140b0aa
idillture <idilture@outlook.com>
2025-12-07 19:57:13 +0300

    Add Gradable interface

---
+++ QuizSystem/src/quiz/Gradable.java
@@ -0,0 +1,13 @@
+package quiz;
+
+public interface Gradable {
+
+	/**
+	 * We use interface for anything that can be graded.
+	 *
+	 * Calculates and returns the score.
+	 */
+	
+	double calculateScore();
+	
+}
