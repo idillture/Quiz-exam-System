0044bbaf8cbfd9fbfdd2c2b7350a73faf863a040
idillture <idilture@outlook.com>
2025-12-12 20:28:14 +0300

    Add Student class extending User

--- QuizSystem/src/quiz/Student.java
+++ QuizSystem/src/quiz/Student.java
@@ -1,33 +1,17 @@
 package quiz;
 
 // A student who takes quizzes in the system.
-public class Student {
-
-	 private String id; 
-	 private String name; 
-	 private double lastScore; // Score of the last quiz taken by the student.
-
-	 public Student(String id, String name) {
-		    this.id = id;
-	        this.name = name;    
-	        this.lastScore = 0.00; // before any quiz is taken.
-	        }
-	 
-	 // Getter
-	 // Returns the id, name, and lastScore of the student.
-	    public String getId() {
-	        return id;
-	    }
-	    public String getName() {
-	        return name;
-	    }
-	    public double getLastScore() {
+public class Student extends User {
+	
+	private double lastScore;
+	
+	public Student(int id, String username, String password, String fullName) {
+        super(id, username, password, fullName);
+        this.lastScore = 0.0;
+	}
+	
+	 public double getLastScore() {
 	        return lastScore;
 	    }
-	     
-	     // Setter
-	     // Updates the lastScore after the student takes a quiz.
-	     public void setLastScore(double lastScore) {
-	        this.lastScore = lastScore;
-	     }
 }
+
