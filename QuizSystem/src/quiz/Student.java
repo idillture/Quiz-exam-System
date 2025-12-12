cc88893093eccc11bc6f3c89fe5759dc702f5e03
idillture <idilture@outlook.com>
2025-12-12 20:33:37 +0300

    Add exam result fields to Student

--- QuizSystem/src/quiz/Student.java
+++ QuizSystem/src/quiz/Student.java
@@ -4,14 +4,29 @@ package quiz;
 public class Student extends User {
 	
 	private double lastScore;
+	private int lastCorrectCount;
+	private int lastWrongCount;
+	
 	
 	public Student(int id, String username, String password, String fullName) {
         super(id, username, password, fullName);
         this.lastScore = 0.0;
+        this.lastCorrectCount = 0;  
+        this.lastWrongCount = 0;  
 	}
 	
 	 public double getLastScore() {
 	        return lastScore;
 	    }
+	 
+	 public int getLastCorrectCount() {
+	        return lastCorrectCount;
+	    }
+
+	    public int getLastWrongCount() {
+	        return lastWrongCount;
+	    }
+	   
+	    
 }
 
