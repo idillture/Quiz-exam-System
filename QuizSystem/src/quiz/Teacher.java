05a90d2bcc803466c02b981cc0e25df361b4d29c
idillture <idilture@outlook.com>
2025-12-15 16:49:45 +0300

    Add toString to Teacher Class

--- QuizSystem/src/quiz/Teacher.java
+++ QuizSystem/src/quiz/Teacher.java
@@ -5,6 +5,13 @@ public class Teacher extends User {
 	 public Teacher(int id, String username, String password, String fullName) {
 
 	        super(id, username, password, fullName);
-
 	 }
+	 
+	 @Override
+	    public String toString() {
+	        return "Teacher:\n" +
+	               "  ID : " + getId() + "\n" +
+	               "  Username : " + getUsername() + "\n" +
+	               "  Full Name : " + getFullName();
+	    }
 }
