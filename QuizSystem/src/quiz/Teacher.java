a0686eb849abd9192508ba0ab792de1b2fc1eade
idillture <idilture@outlook.com>
2025-12-15 16:47:47 +0300

    Add Teacher class

---
+++ QuizSystem/src/quiz/Teacher.java
@@ -0,0 +1,10 @@
+package quiz;
+
+public class Teacher extends User {
+	
+	 public Teacher(int id, String username, String password, String fullName) {
+
+	        super(id, username, password, fullName);
+
+	 }
+}
