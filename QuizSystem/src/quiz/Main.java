3af2f2ad4aee7d652c6e50b315b142d00f4a59fc
idillture <idilture@outlook.com>
2025-12-25 14:20:29 +0300

    Updated Main Class

--- QuizSystem/src/quiz/Main.java
+++ QuizSystem/src/quiz/Main.java
@@ -2,6 +2,7 @@ package quiz;
 
 public class Main {
     public static void main(String[] args) {
-        QuizSystem.main(args);
+        QuizSystem system = new QuizSystem();
+        system.start();
     }
 }
