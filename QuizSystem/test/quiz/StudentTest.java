eabe252fb828fda6caa5ab926827a612817777f7
idillture <idilture@outlook.com>
2025-12-28 21:25:32 +0300

    update StudentTest

--- QuizSystem/test/quiz/StudentTest.java
+++ QuizSystem/test/quiz/StudentTest.java
@@ -4,19 +4,66 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class StudentTest {
-	
-	    @Test
-	    void lastExamResultsAreStoredCorrectly() {
-	        Student s = new Student(1, "user", "123", "Test User");
 
-	        s.setLastScore(8.5);
-	        s.setLastCorrectCount(6);
-	        s.setLastWrongCount(4);
+    @Test
+    void constructorSetsFieldsCorrectly() {
 
-	        assertEquals(8.5, s.getLastScore());
-	        assertEquals(6, s.getLastCorrectCount());
-	        assertEquals(4, s.getLastWrongCount());
-	    }
-	}
+        Student student = new Student(
+                101,
+                "idilture",
+                "123",
+                "İdil Türe"
+        );
 
+        assertEquals(101, student.getId());
+        assertEquals("idilture", student.getUsername());
+        assertEquals("İdil Türe", student.getFullName());
+    }
 
+    @Test
+    void checkPasswordReturnsTrueForCorrectPassword() {
+
+        Student student = new Student(
+                101,
+                "idilture",
+                "123",
+                "İdil Türe"
+        );
+
+        assertTrue(student.checkPassword("123"));
+    }
+
+    @Test
+    void checkPasswordReturnsFalseForWrongPassword() {
+
+        Student student = new Student(
+                101,
+                "idilture",
+                "123",
+                "İdil Türe"
+        );
+
+        assertFalse(student.checkPassword("12"));
+        assertFalse(student.checkPassword("abc"));
+        assertFalse(student.checkPassword(""));
+    }
+
+    @Test
+    void examResultsAreStoredCorrectly() {
+
+        Student student = new Student(
+                101,
+                "idilture",
+                "123",
+                "İdil Türe"
+        );
+
+        student.setLastScore(90.0);
+        student.setLastCorrectCount(8);
+        student.setLastWrongCount(2);
+
+        assertEquals(90.0, student.getLastScore());
+        assertEquals(8, student.getLastCorrectCount());
+        assertEquals(2, student.getLastWrongCount());
+    }
+}
