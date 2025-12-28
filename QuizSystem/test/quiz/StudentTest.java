1e6f72ccc355860441a4cdd7748ff23e2082043a
idillture <idilture@outlook.com>
2025-12-28 20:30:28 +0300

    added quiz.StudentTest

--- QuizSystem/test/quiz/StudentTest.java
+++ QuizSystem/test/quiz/StudentTest.java
@@ -1,5 +1,22 @@
 package quiz;
 
-public class StudentTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-}
+class StudentTest {
+	
+	    @Test
+	    void lastExamResultsAreStoredCorrectly() {
+	        Student s = new Student(1, "user", "123", "Test User");
+
+	        s.setLastScore(8.5);
+	        s.setLastCorrectCount(6);
+	        s.setLastWrongCount(4);
+
+	        assertEquals(8.5, s.getLastScore());
+	        assertEquals(6, s.getLastCorrectCount());
+	        assertEquals(4, s.getLastWrongCount());
+	    }
+	}
+
+
